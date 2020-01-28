ee48ea883327350e51fbe2af156ec9eb0d241c7d
dasolovkin <dasolovkinne@mail.ru>
2020-01-29 01:35:00 +0300

    Решение третьей задачи раздела "Занятие 1: типы, let и const" - Сделать первый символ заглавным

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,4 +4,11 @@
  * @returns {string}
  */
 function ucFirst(str) {
+    if (!str){
+        return str;
+    }
+    
+    str = str.trim();
+
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
