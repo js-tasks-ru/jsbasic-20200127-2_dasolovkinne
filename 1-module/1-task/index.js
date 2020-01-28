7c41626c0273641364831d7ffeba86a0ff72c1ef
dasolovkin <dasolovkinne@mail.ru>
2020-01-29 00:42:34 +0300

    Решение первой задачи раздела "Занятие 1: типы, let и const" - Вычислить факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,14 @@
  * @returns {number}
  */
 function factorial(n) {
+    "use strict";
+        
+    let result = n > 0 ? n : 1;
 
+    while (n > 1){
+        result *= --n;        
+    }
+    
+    return result;
 }
+
