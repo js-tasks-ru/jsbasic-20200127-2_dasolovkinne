5003170adce29e0def960db51a343e641bf74936
dasolovkin <dasolovkinne@mail.ru>
2020-01-29 01:53:16 +0300

    Решение четвертой задачи раздела "Занятие 1: типы, let и const" - Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,8 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
+    str = str || "";
+    let strUpperCase = str.toUpperCase();
 
+    return  strUpperCase.includes('1xBet'.toUpperCase()) || strUpperCase.includes('XXX') 
 }
