ebd9a7f694fcb88dbef7874321e8c954adc47807
dasolovkin <60316610+dasolovkin@users.noreply.github.com>
2020-01-29 23:44:05 +0300

    Revert "Тест"

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,13 +5,5 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-    if (!str){
-        return str;
-    }
-        
-    return maxlength > 0
-        ? str.length > maxlength
-            ? str.substr(0, maxlength - 1) + "…"
-            : str
-        : "";
+
 }
