e7dcd633cd20c3797e251504d9f57801e7cf8e60
dasolovkin <dasolovkinne@mail.ru>
2020-02-13 13:55:16 +0300

    Занятие 5: Обработчики событий - Компонент Сортируемая таблица (SortableTable)

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -18,6 +18,20 @@ function SortableTable(items) {
    * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
   this.el = document.createElement('table');
+  
+  let hteadElement = document.createElement('thead');
+  this.el.append(hteadElement);
+
+  let tdElement = createTrElement('Name', 'Age', 'Salary', 'City');
+  hteadElement.append(tdElement);
+
+  let tbodyElement = document.createElement('tbody');
+  this.el.append(tbodyElement);
+  
+  for(let item of items){
+    let trElement = createTrElement(item.name, item.age, item.salary, item.city);
+    tbodyElement.append(trElement);
+  }  
 
   /**
    * Метод выполняет сортировку таблицы
@@ -25,5 +39,36 @@ function SortableTable(items) {
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+  this.sort = function(column, desc = false) {    
+    let tbodyElement = this.el.querySelector('tbody');    
+    let rowsArray = Array.from(tbodyElement.rows);
+
+    rowsArray.sort((row1, row2) => {
+      if (column == 0 || column == 3){
+        let sortResult = desc 
+          ? row2.cells[column].innerHTML > row1.cells[column].innerHTML 
+          : row1.cells[column].innerHTML > row2.cells[column].innerHTML;
+        return sortResult ? 1 : -1;
+      } else {        
+        return desc 
+          ? row2.cells[column].innerHTML - row1.cells[column].innerHTML
+          : row1.cells[column].innerHTML - row2.cells[column].innerHTML;                
+      }
+      
+    });
+    
+    tbodyElement.append(...rowsArray);
+  };
+}
+
+function createTrElement(...cellInfo){  
+  let trElement = document.createElement('tr');
+
+  for (let cellInfoItem of cellInfo){    
+    let tdElement = document.createElement('td');
+    tdElement.innerHTML = cellInfoItem;
+    trElement.append(tdElement);
+  }
+
+  return trElement;
 }
